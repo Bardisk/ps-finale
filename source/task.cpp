6aeaa6d11540f03be2752c5431ea8f7390008a86
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-07 21:36:07 +0800

    compile

--- source/task.cpp
+++ source/task.cpp
@@ -1,6 +1,9 @@
 #include <basics.h>
 
 namespace Mainctr {
+  Command::CommandType command_p1, command_p2;
+  Direction::DirectionKind direction_p1, direction_p2;
+  
   void getDecision() {
     command_p1 = Command::Move;
     command_p2 = Command::Move;
