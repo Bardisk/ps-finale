bfb309ec4a59f0d49d3b0758e8644e9374bd183f
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-07 21:37:01 +0800

    compile

--- include/task/Mainctr.h
+++ include/task/Mainctr.h
@@ -9,8 +9,8 @@ namespace Mainctr {
     WAITFOR,
   }state_p1, state_p2;
   
-  Command::CommandType command_p1, command_p2;
-  Direction::DirectionKind direction_p1, direction_p2;
+  extern Command::CommandType command_p1, command_p2;
+  extern Direction::DirectionKind direction_p1, direction_p2;
   inline std::string respond() {
     std::stringstream ss;
     ss << Command::encode(command_p1) << " " << Direction::encode(direction_p1) << '\n' << Command::encode(command_p2) << Direction::encode(direction_p2) << '\n';
