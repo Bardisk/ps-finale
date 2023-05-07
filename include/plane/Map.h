f0a5e365fdb60b29f5cab128dbfe5d69a15f94e0
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-07 21:26:57 +0800

    compile

--- include/plane/Map.h
+++ include/plane/Map.h
@@ -8,7 +8,7 @@ namespace Map{
     T data[N_MAX * N_SCALE][M_MAX * M_SCALE];
     struct LocalLocation : public Location {
       Matrix *father;
-      LocalLocation(int _x=0, int _y=0, const Matrix *_f=NULL) : Location(_x, _y), father(_f) {}
+      LocalLocation(int _x=0, int _y=0, Matrix *_f=NULL) : Location(_x, _y), father(_f) {}
       LocalLocation(Location location) : Location(location) {} 
       LocalLocation operator ++(int) {
         if (y < m * M_SCALE) {
@@ -42,10 +42,10 @@ namespace Map{
 
     //stl_container-like
     using iterator = LocalLocation;
-    LocalLocation begin() const {
+    LocalLocation begin() {
       return LocalLocation(0, 0, this);
     }
-    LocalLocation end() const {
+    LocalLocation end() {
       return LocalLocation(n * N_SCALE, 0, this);
     }
   };
